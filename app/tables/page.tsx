2e9471d23b76929d2362124c0f959ec7ef789116
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-19 01:29:41 +0530

    Refactor TablesPage component for improved code formatting and readability

--- app/tables/page.tsx
+++ app/tables/page.tsx
@@ -6,16 +6,16 @@ import { Footer } from "../../components/footer"
 import { TableManagement } from "../../components/table-management"
 
 export default function TablesPage() {
-  return (
-    <div className="flex h-screen bg-gray-50">
-      <SidebarNav />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <Header />
-        <div className="flex-1 overflow-auto p-6">
-          <TableManagement />
+    return (
+        <div className="flex h-screen bg-gray-50">
+            <SidebarNav />
+            <div className="flex-1 flex flex-col overflow-hidden">
+                <Header />
+                <div className="flex-1 overflow-auto p-6">
+                    <TableManagement />
+                </div>
+                <Footer />
+            </div>
         </div>
-        <Footer />
-      </div>
-    </div>
-  )
+    )
 }
