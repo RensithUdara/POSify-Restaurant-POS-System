411e0207f77fe86a08bcdc00cd7c639682b2232d
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-19 19:54:11 +0530

    Add ReservationsPage component with layout and basic content

---
+++ app/reservations/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import React from "react"
+import { SidebarNav } from "../../components/sidebar-nav"
+import { Header } from "../../components/header"
+import { Footer } from "../../components/footer"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+
+export default function ReservationsPage() {
+  return (
+    <div className="min-h-screen md:flex">
+      <aside className="w-72 hidden md:block">
+        <SidebarNav />
+      </aside>
+
+      <main className="flex-1 p-6">
+        <Header />
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Reservations</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Reservations and bookings will be managed here.</p>
+          </CardContent>
+        </Card>
+
+        <Footer />
+      </main>
+    </div>
+  )
+}
