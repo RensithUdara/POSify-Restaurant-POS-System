9c5aab6ba0f0fbfa406a5dc781ed1af501b54a51
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-19 19:54:40 +0530

    Refactor ReservationsPage component for improved code formatting and readability

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -7,26 +7,26 @@ import { Footer } from "../../components/footer"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 export default function ReservationsPage() {
-  return (
-    <div className="min-h-screen md:flex">
-      <aside className="w-72 hidden md:block">
-        <SidebarNav />
-      </aside>
+    return (
+        <div className="min-h-screen md:flex">
+            <aside className="w-72 hidden md:block">
+                <SidebarNav />
+            </aside>
 
-      <main className="flex-1 p-6">
-        <Header />
+            <main className="flex-1 p-6">
+                <Header />
 
-        <Card>
-          <CardHeader>
-            <CardTitle>Reservations</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>Reservations and bookings will be managed here.</p>
-          </CardContent>
-        </Card>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Reservations</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <p>Reservations and bookings will be managed here.</p>
+                    </CardContent>
+                </Card>
 
-        <Footer />
-      </main>
-    </div>
-  )
+                <Footer />
+            </main>
+        </div>
+    )
 }
